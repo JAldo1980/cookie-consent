33fa7e747467865f42dfa99cd8040f5338e7369e
James Alderman <80925002+JAldo1980@users.noreply.github.com>
2023-01-31 17:00:27 +0000

    JS Update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const modalCloseBtn = document.getElementById("modal-close-btn");
 
 setTimeout(function () {
   modal.style.display = "inline";
-}, 1500f);
+}, 1500);
 
 modalCloseBtn.addEventListener("click", function () {
   modal.style.display = "none";
